5338a648d0232fd10279d811c01da96a9c78a6c0
Raphael Oliveira <raphael.oliveira@beemit.com.au>
2018-10-12 09:29:38 +1100

    Add cocoapods

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.4'
+use_frameworks!
+
+target 'CocoapodsBuildConfiguration' do
+    pod 'Alamofire', '~> 4.7', :configuration => ['Release']
+end
+
---
+++ Pods/Target Support Files/Pods-CocoapodsBuildConfiguration/Pods-CocoapodsBuildConfiguration-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocoapodsBuildConfiguration : NSObject
+@end
+@implementation PodsDummy_Pods_CocoapodsBuildConfiguration
+@end
